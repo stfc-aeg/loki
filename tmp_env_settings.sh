36d85945446c53fc22961988341a2cecd497cbcb
Joseph Nobes <joseph.nobes@stfc.ac.uk>
2023-02-17 14:18:18 +0000

    Added beginnings of alternative carrier support
    There will be base support block designs and constraints structures used for different carriers
    to provide a working image. This will later be supplemented later on (in the application starter
    kit) with additional application constraints files, and the option to override the default block
    design and constraints directory if desired, though ideally this should not be required for most
    basic applications. This will require updates to the block design to allow it to provide more
    generic functionality.

--- tmp_env_settings.sh
+++ tmp_env_settings.sh
@@ -3,3 +3,6 @@
 export PLATFORM=4cg_2gb
 export HWSW_PATH=./design/
 export OS_PATH=./os/petalinux-custom/
+
+# This will be used to select which constraints and board design files are used
+export CARRIER=tebf0808

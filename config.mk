5f498e08c6bdc2b18b1f81ed9eb65ecc1b108669
Joseph Nobes <joseph.nobes@stfc.ac.uk>
2023-03-24 09:22:26 +0000

    Added rules, fixed prefix issue with making Makfiles with autoconf
    Autoconf was failing to make some Makefiles if they did not exist, which turned out to be
    due to my attempt to use several rules for the same recipe in a manner that did not make
    sense, but didn't cause an error. This has now been corrected.
    
    During this process the prefix handling has been improved a bit in the configuration
    makefile, config.mk.
    
    A few rules for cleanup have been tweaked.

--- config.mk
+++ config.mk
@@ -8,19 +8,33 @@
 #
 # This file will generate the main LOKI project Makefile from Makefile.in
 
-AUTOCONF_OUTPUTS=$(addprefix ${LOKI_DIR}, Makefile)
-AUTOCONF_INPUTS=$(addprefix ${LOKI_DIR}, Makefile.in configure)
+AUTOCONF_OUTPUTS= Makefile
+AUTOCONF_INPUTS= Makefile.in configure
 
-AUTOCONF_HW_OUTPUTS=$(addprefix ${LOKI_DIR}, design/Makefile design/design_basic_settings.sh)
-AUTOCONF_HW_INTPUTS=$(addprefix ${LOKI_DIR}, design/Makefile.in design/design_basic_settings.sh.in)
+AUTOCONF_HW_OUTPUTS= design/Makefile design/design_basic_settings.sh
+AUTOCONF_HW_INTPUTS= design/Makefile.in design/design_basic_settings.sh.in
 
 # Currently SW is part of the design, and relies on the same files. These variables
 # are preserved in case software-specific autoconf-generated files are later required.
-AUTOCONF_SW_OUTPUTS=$(addprefix ${LOKI_DIR}, ${AUTOCONF_HW_OUTPUTS})
-AUTOCONF_SW_INPUTS=$(addprefix ${LOKI_DIR}, ${AUTOCONF_HW_INPUTS})
+AUTOCONF_SW_OUTPUTS= ${AUTOCONF_HW_OUTPUTS}
+AUTOCONF_SW_INPUTS= ${AUTOCONF_HW_INPUTS}
 
-AUTOCONF_OS_OUTPUTS=$(addprefix ${LOKI_DIR}, os/petalinux-custom/Makefile os/petalinux-custom/project-spec/configs/config)
-AUTOCONF_OS_INPUTS=$(addprefix ${LOKI_DIR}, os/petalinux-custom/Makefile.in os/petalinux-custom/project-spec/configs/config.in)
+AUTOCONF_OS_OUTPUTS= os/petalinux-custom/Makefile os/petalinux-custom/project-spec/configs/config
+AUTOCONF_OS_INPUTS= os/petalinux-custom/Makefile.in os/petalinux-custom/project-spec/configs/config.in
+
+# Make these paths relative to the calling makefile's location
+AUTOCONF_OUTPUTS_PF=$(addprefix ${LOKI_DIR}, ${AUTOCONF_OUTPUTS})
+AUTOCONF_INPUTS_PF=$(addprefix ${LOKI_DIR}, ${AUTOCONF_INPUTS})
+AUTOCONF_HW_OUTPUTS_PF=$(addprefix ${LOKI_DIR}, ${AUTOCONF_HW_OUTPUTS})
+AUTOCONF_HW_INTPUTS_PF=$(addprefix ${LOKI_DIR}, ${AUTOCONF_HW_INTPUTS})
+AUTOCONF_SW_OUTPUTS_PF=$(addprefix ${LOKI_DIR}, ${AUTOCONF_SW_OUTPUTS})
+AUTOCONF_SW_INPUTS_PF=$(addprefix ${LOKI_DIR}, ${AUTOCONF_SW_INPUTS})
+AUTOCONF_OS_OUTPUTS_PF=$(addprefix ${LOKI_DIR}, ${AUTOCONF_OS_OUTPUTS})
+AUTOCONF_OS_INPUTS_PF=$(addprefix ${LOKI_DIR}, ${AUTOCONF_OS_INPUTS})
+
+$(info autoconf configuring LOKI project at $$LOKI_DIR: ${LOKI_DIR})
+$(info $$AUTOCONF_PARAMS are ${AUTOCONF_PARAMS})
+$(info $$AUTOCONF_HW_OUTPUTS are ${AUTOCONF_HW_OUTPUTS_PF})
 
 .PHONY: loki-configure-hw loki-configure-sw loki-configure-os loki-configure-help
 
@@ -28,19 +42,21 @@ AUTOCONF_OS_INPUTS=$(addprefix ${LOKI_DIR}, os/petalinux-custom/Makefile.in os/p
 # the autoconf will rebuild that target file and will not rebuild the unrelated
 # outputs of the ./configure, preventing unnecessary rebuilding of other parts
 # of the project that haven't actually been reconfigured.
-${AUTOCONF_HW_OUTPUTS}: ${AUTOCONF_HW_INPUTS} ${AUTOCONF_INPUTS}
-${AUTOCONF_SW_OUTPUTS}: ${AUTOCONF_SW_INPUTS} ${AUTOCONF_INPUTS}
-${AUTOCONF_OS_OUTPUTS}: ${AUTOCONF_OS_INPUTS} ${AUTOCONF_INPUTS}
+${AUTOCONF_HW_OUTPUTS_PF}: ${AUTOCONF_HW_INPUTS_PF} ${AUTOCONF_INPUTS_PF}
+${AUTOCONF_SW_OUTPUTS_PF}: ${AUTOCONF_SW_INPUTS_PF} ${AUTOCONF_INPUTS_PF}
+${AUTOCONF_OS_OUTPUTS_PF}: ${AUTOCONF_OS_INPUTS_PF} ${AUTOCONF_INPUTS_PF}
+$(AUTOCONF_OUTPUTS_PF): $(AUTOCONF_INPUTS_PF)
+$(AUTOCONF_HW_OUTPUTS_PF) ${AUTOCONF_SW_OUTPUTS_PF} ${AUTOCONF_OS_OUTPUTS_PF} $(AUTOCONF_OUTPUTS_PF): $(AUTOCONF_INPUTS_PF)
 	echo "Configuring LOKI with parameters: ${AUTOCONF_PARAMS}"
 	cd ${LOKI_DIR}; ./configure --prefix=$(shell pwd)/${LOKI_DIR} \
 		${AUTOCONF_PARAMS}
 
 # Call from other (application ) makefiles without knowing the files autoconf depends on
-loki-configure-hw: ${AUTOCONF_HW_OUTPUTS}
+loki-configure-hw: ${AUTOCONF_HW_OUTPUTS_PF} ${AUTOCONF_OUTPUTS_PF}
 
-loki-configure-sw: ${AUTOCONF_SW_OUTPUTS}
+loki-configure-sw: ${AUTOCONF_SW_OUTPUTS_PF} ${AUTOCONF_OUTPUTS_PF}
 
-loki-configure-os: ${AUTOCONF_OS_OUTPUTS}
+loki-configure-os: ${AUTOCONF_OS_OUTPUTS_PF} ${AUTOCONF_OUTPUTS_PF}
 
 loki-configure-help:
 	cd ${LOKI_DIR}; ./configure --help=short

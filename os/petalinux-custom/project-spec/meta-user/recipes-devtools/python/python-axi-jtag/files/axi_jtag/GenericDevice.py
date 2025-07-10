d1e68e096c2f586ba954078c64b8094335f5e4e6
CallumLee22 <callum.lee@stfc.ac.uk>
2025-07-10 16:07:25 +0100

    Allow file not to be provided

--- os/petalinux-custom/project-spec/meta-user/recipes-devtools/python/python-axi-jtag/files/axi_jtag/GenericDevice.py
+++ os/petalinux-custom/project-spec/meta-user/recipes-devtools/python/python-axi-jtag/files/axi_jtag/GenericDevice.py
@@ -1,18 +1,40 @@
 import json
 import os
+import re
+
+class InstructionFileNotProvidedException(Exception):
+    """
+    Exception for when an instruction name is provided, but no
+    instructions file has been provided
+    """
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
 
 class GenericDevice():
 
-    def __init__(self, ir_length: int, instructions_file: str):
+    def __init__(self, ir_length: int, instructions_file: str=None):
         self.ir_length = ir_length
+        self.instructions_file = instructions_file
 
-        base_dir = os.path.dirname(os.path.abspath(__file__))
-        instructions_file_path = os.path.join(base_dir, "device_instructions", instructions_file)
-        with open(instructions_file_path) as json_file:
-            self.instructions = json.load(json_file)
+        if self.instructions_file:
+            base_dir = os.path.dirname(os.path.abspath(__file__))
+            instructions_file_path = os.path.join(base_dir, "device_instructions", self.instructions_file)
+            with open(instructions_file_path) as json_file:
+                self.instructions = json.load(json_file)
     
     def set_chain(self, chain):
         self.chain = chain
     
     def shift_ir(self, instruction: str):
-        self.chain.shift_ir(self, self.instructions[instruction])
+        if not re.search("[a-zA-Z]", instruction):
+            instruction_code = instruction
+        else:
+            if not self.instructions:
+                raise InstructionFileNotProvidedException(
+                    "No instructions file provided, please provide one or provide the instruction code"
+                    )
+            
+            instruction_code = self.instructions[instruction]
+
+        self.chain.shift_ir(self, instruction_code)
